9115bf241a5d2d25ae56762bdd956a541b69c71e
Karan2689 <60309619+Karan2689@users.noreply.github.com>
2023-04-29 17:40:10 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-function showpwd()
-{
-  document.getElementById("pwd").type="text";
-  document.getElementById("cpwd").type="text";
-}
+$(document).on('submit','#userForm',function(e){
+                        e.preventDefault();
+                        $.ajax({
+                        method:"POST",
+                        url: "php/index.php",
+                        data:$(this).serialize(),
+                        success: function(data){
+                          if(data="Successful")
+                          {
+                            alert("Profile creation Successful");
+                            localStorage.setItem("status", "out");
+                            window.location.href = 'login.html';
+                          }
+                          else if(data="Unsuccessful")
+                          {
+                            alert("Profile creation Unsuccessful");
+                          }
+                          console.log(data);
+                        // $('#msg').html(data);
+                        $('#userForm').find('input').val('')}
+                      });
+                });
